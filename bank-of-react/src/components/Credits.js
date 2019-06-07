ba368fb254cbd8393aa896b2648b1159adeb0f87
Karanveer Singh <karansingh98123@gmail.com>
2019-06-06 20:06:08 -0400

    finished with this component

--- bank-of-react/src/components/Credits.js
+++ bank-of-react/src/components/Credits.js
@@ -7,34 +7,16 @@ class Credits extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            amount: 0,
-            description: ''
+            amount: 0.0,
+            description: '',
         };
     }
 
     onChange = (event) => {
 
         this.setState({
-
             [event.target.name]: event.target.value
         });
-    }
-
-    onSubmit = () => {
-        this.addNewItem();
-    }
-
-    addNewItem = () => {
-
-        let today = new Date();
-        let dd = String(today.getDate()).padStart(2, '0');
-        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        let yyyy = today.getFullYear();
-
-        today = mm + '/' + dd + '/' + yyyy;
-        return(
-            <CreditView amount={this.state.amount} description={this.state.description} date={today}/>
-        );
     }
 
     render() {
@@ -43,22 +25,26 @@ class Credits extends Component {
                 <title>
                     Credits
                 </title>
+
                 <Link to="/"> Home </Link>
                 <h4> <AccountBalance accountBalance={this.props.balance} /> </h4>
                 <h1> Credits </h1>
 
                 <form>
-                    Amount
+                    Amount:
                     <input type="number" name="amount" onChange={this.onChange}/> <br/>
-                    Description
+                    Description:
                     <input type="text" name="description" onChange={this.onChange}/> <br/>
                 </form>
-                <button onSubmit={this.onSubmit}> Add </button>
+
+                <button onClick={() => this.props.addCredit(this.state.amount, this.state.description)}>
+                    Add
+                </button>
 
                 {this.props.creditData.map(creditData =>
                     <div key={creditData.id}>
-                        <CreditView description={creditData.description} amount={creditData.amount}
-                        date={creditData.date} />
+                        <CreditView description={creditData.description}
+                        amount={creditData.amount} date={creditData.date} />
                     </div>
                 )}
             </div>
